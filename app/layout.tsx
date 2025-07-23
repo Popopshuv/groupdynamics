1d7edaf24aaaba8e8d5181b02114167942a4e7b6
IB - Scott <scott@impossible-bureau.com>
2025-07-23 16:11:35 -0600

    update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,24 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "group dynamics",
   description: "group dynamics",
+  openGraph: {
+    title: "group dynamics",
+    description: "group dynamics",
+    images: [
+      {
+        url: "/images/groupd-meta.svg",
+        width: 1200,
+        height: 630,
+        alt: "Group Dynamics",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "group dynamics",
+    description: "group dynamics",
+    images: ["/images/groupd-meta.png"],
+  },
 };
 
 export default function RootLayout({
