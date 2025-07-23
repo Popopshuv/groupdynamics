6e63e356e75d1950a96894671d4928cf6fda4505
IB - Scott <scott@impossible-bureau.com>
2025-07-23 16:22:18 -0600

    remove overscroll

--- components/ScrollingPage/styles.ts
+++ components/ScrollingPage/styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 500vh;
+  height: 100vh;
+  width: 100vw;
   overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
   /* cursor: none; */
   /* transition: height 1s ease-in-out; */
 
   &.intro-complete {
-    height: 500vh;
+    height: 100vh;
   }
 `;
